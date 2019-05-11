550597720a0d30ca8f684912dfa696d0fd49deab
Aravind Raj <aravinddevaraj777@gmail.com>
2019-05-11 15:34:23 +0530

    Initial commit

---
+++ App_Code/IRestBasedJsonWebService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+[ServiceContract]
+public interface IRestBasedJsonWebService
+{
+    [OperationContract]
+    [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/welcome/{myname}")]  //the name specified in UriTemplate must match the parameter name eg.. myname
+    Student Welcome(string myname);
+
+}
+[DataContract]
+public class Student
+{
+    [DataMember]
+    public string Name  //The name of property is Name
+    {
+        get; //to get value of the property
+        set; //to set value for the property
+    }
+}
---
+++ App_Code/RestBasedJsonWebService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+public class RestBasedJsonWebService : IRestBasedJsonWebService
+{
+    // returns a welcome message
+    public Student Welcome(string yourName)
+    {
+        // add welcome message to field of TextMessage object
+        Student obj = new Student();
+        obj.Name = yourName;
+        return obj;
+    } // end method Welcome    
+}
